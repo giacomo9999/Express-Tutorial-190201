64093c68a29d8804f591619a1af4243079132008
James Gary <gmarchesi888@gmail.com>
2019-02-01 13:20:13 -0500

    Routes set up.

--- controllers/bookEntry.controller.js
+++ controllers/bookEntry.controller.js
@@ -1,5 +1,17 @@
+const BookEntry = require("../models/bookEntry.model");
+
+// Display root message ("bookEntries")
+exports.nothing = (req, res) =>
+  res.send("Greetings From bookEntry Controller Nothing.");
+
+// Display test message ("bookEntries/test")
 exports.test = (req, res) =>
   res.send("Greetings From bookEntry Controller Test.");
 
-exports.nothing = (req, res) =>
-  res.send("Greetings From bookEntry Controller Nothing.");
+// Display list of all entries
+exports.display_all = (req, res) =>
+  res.send("NOT IMPLEMENTED: List of all entries");
+
+// Post new entry
+exports.post_new_entry = (req, res) =>
+  res.send("NOT IMPLEMENTED: Post new entry");
--- routes/bookEntry.route.js
+++ routes/bookEntry.route.js
@@ -6,5 +6,7 @@ const bookEntry_controller = require("../controllers/bookEntry.controller");
 /* GET home page. */
 router.get("/", bookEntry_controller.nothing);
 router.get("/test", bookEntry_controller.test);
+router.get("/all", bookEntry_controller.display_all);
+router.post("/new", bookEntry_controller.post_new_entry);
 
 module.exports = router;
