83985e956759173c49f336196d67e158a2b3edb2
James Gary <gmarchesi888@gmail.com>
2019-02-01 10:13:22 -0500

    Added nodemon and edited README.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Welcome to Express.");
+  res.send("Jim Says: Welcome to Express.");
 });
 
 module.exports = router;
