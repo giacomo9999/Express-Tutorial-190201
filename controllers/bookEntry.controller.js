9afd8be721fd4da638ecc6ac28859c46c22de222
James Gary <gmarchesi888@gmail.com>
2019-02-01 14:15:40 -0500

    Add new entry now working.

--- controllers/bookEntry.controller.js
+++ controllers/bookEntry.controller.js
@@ -13,5 +13,15 @@ exports.display_all = (req, res) =>
   res.send("NOT IMPLEMENTED: List of all entries");
 
 // Post new entry
-exports.post_new_entry = (req, res) =>
-  res.send("NOT IMPLEMENTED: Post new entry");
+exports.post_new_entry = (req, res, next) => {
+  let newEntry = new BookEntry({
+    guestSignature: req.body.signature,
+    message: req.body.message
+  });
+  newEntry.save(err => {
+    if (err) {
+      return next(err);
+    }
+    res.send("Entry added successfully.");
+  });
+};
